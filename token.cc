4e63d318758fccf3de056fdb08f357c6692c2e52
sksat <sksat.tec@gmail.com>
2018-05-08 21:39:10 +0900

    [UPDATE] tokenizer

--- token.cc
+++ token.cc
@@ -12,18 +12,69 @@ void skip_space(std::string_view &src){
 	src.remove_prefix(skip);
 }
 
+bool skip_comment(std::string_view &src){
+	size_t skip;
+	if(src[0] != '/') return false;
+	if(src[1] == '/'){ // 1行コメント
+		for(skip=2;skip<=src.size();skip++){
+			if(src[skip] == '\n') break;
+		}
+	}else if(src[1] == '*'){ // 複数行コメント
+		for(skip=2;skip<=src.size();skip++){
+			const char &c = src[skip];
+			if(src[skip-1]=='*' && c == '/') break;
+		}
+	}else return false;
+	src.remove_prefix(skip+1);
+	return true;
+}
+
 token_t get_delimiter_token(std::string_view &src){
 	token_t t;
 	size_t siz;
 	switch(src[0]){
-		// 空白
+		// 文字列
 		case '\'':
 		case '\"':
 			siz = src.find(src[0], 1);
 			if(siz == std::string_view::npos){ /*TODO: error*/ }
 			siz++;
 			break;
-		case ';':
+		case ':': // ':' or '::'
+			siz = 1;
+			if(src[1] == ':') siz++;
+			break;
+		case '<': // <,<<,<=,<<=
+		case '>': // >,>>,>=,>>=
+			siz = 1;
+			if(src[1] == src[0]) siz++; // <<,>>
+			if(src[siz] == '=') siz++; // <=,>=,<<=,>>=
+			break;
+		case '-': // -,-=,->
+		case '=': // =,==,=>
+			siz = 1;
+			if(src[1]=='=' || src[1]=='>') siz++;
+			break;
+		case '+': // +,++,+=
+		case '&': // &,&&,&=
+		case '|': // |,||,|=
+			siz = 1;
+			if(src[0] == src[1] || src[1] == '=') siz++;
+			break;
+		case '*': // *,*=
+		case '/': // /,/=
+		case '%': // %,%=
+		case '!': // !,!=
+		case '^': // ^,^=
+			siz = 1;
+			if(src[1] == '=') siz++;
+			break;
+		// 確実に1字で区切れるやつ
+		case '[': case ']':
+		case '{': case '}':
+		case '(': case ')':
+		case ',': case ';':
+		case '~': case '?':
 			siz = 1;
 			break;
 		default:
@@ -42,16 +93,29 @@ token_t get_token(std::string_view &src){
 	token_t t;
 	size_t siz = 0;
 
-	skip_space(src);
+	while(true){
+		skip_space(src);
+		if(!skip_comment(src)) break;
+	}
 	if(src.empty())
 		return {};
 
 	while(true){
 		switch(src[siz]){
+			case ' ': case '\t': case '\n':
 			// 区切り文字
 			case '\'':
 			case '\"':
-			case ';':
+			case ':':
+			case '<': case '>':
+			case '-': case '=':
+			case '+': case '&': case '|':
+			case '*': case '/': case '%': case '!': case '^':
+			case '[': case ']':
+			case '{': case '}':
+			case '(': case ')':
+			case ',': case ';':
+			case '~': case '?':
 				if(siz==0) t = get_delimiter_token(src);
 				else{
 					t.s = src.substr(0, siz); // 直前まで
